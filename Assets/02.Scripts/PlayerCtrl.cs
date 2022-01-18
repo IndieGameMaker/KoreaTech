7c3581a4c2c6795abe1e52a27bd3fecff3f2093d
JaeHyun Lee <myleje@gmail.com>
2022-01-18 14:38:41 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private float v;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,9 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        v = Input.GetAxis("Vertical"); // -1.0f ~ 0.0f ~ +1.0f
+        Debug.Log("v=" + v); // print("v=" + v);
+
         //transform.position += new Vector3(0, 0, 0.1f);
         transform.Translate(Vector3.forward * 0.1f);
 
